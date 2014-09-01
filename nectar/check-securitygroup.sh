7c4d59beb1712f8cfe3ba3a6354f2f29d9cd9f9f
sirswa[at]gmail.com <swe.aung@monash.edu>
2014-09-02 00:37:23 +1000

    change security group script

--- nectar/check-securitygroup.sh
+++ nectar/check-securitygroup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in `nova list-secgroup $1 | grep -vE '(Id|\+)' | awk '{print $2}'`;do nova secgroup-list-rules $i;done
+for i in `nova list-secgroup $1 | grep -vE '(Id|\+)' | awk '{print $2}'`;do echo Securty Group ID: $i;nova secgroup-list-rules $i;done
